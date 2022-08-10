ab3be62189a0c4ac4a3d62151a8e40edfaac1fa1
mohamedMallahi <kingcoding17@gmail.com>
2022-08-10 11:06:11 +0100

    2 files modified

--- pages/index.js
+++ pages/index.js
@@ -4,10 +4,40 @@ import Link from 'next/link';
 import Navbar from '../components/Navbar';
 import Menu from '../components/Menu';
 import { useAuth } from '../contexts/AuthContext';
-// import styles from '../styles/Home.module.css';
+
+const DUMMY_DB = [
+  {
+    image:
+      'https://images.pexels.com/photos/4490130/pexels-photo-4490130.jpeg?auto=compress&cs=tinysrgb&w=400',
+    caption: 'Post Number One',
+    stats: {
+      likes: 2,
+      comments: ['Hello'],
+    },
+  },
+  {
+    image:
+      'https://images.pexels.com/photos/15286/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=400',
+    caption: 'Post Number Two',
+    stats: {
+      likes: 6,
+      comments: ['Hello', 'What', 'StandUp'],
+    },
+  },
+  {
+    image:
+      'https://images.pexels.com/photos/624015/pexels-photo-624015.jpeg?auto=compress&cs=tinysrgb&w=400',
+    caption: 'Post Number Three',
+    stats: {
+      likes: 426,
+      comments: ['Hello', 'What', 'StandUp', 'H', 'hh'],
+    },
+  },
+];
 
 export default function Home() {
   const { user } = useAuth();
+
   React.useEffect(() => {
     if (user) {
       console.log('Authenticated Successfully !');
@@ -15,6 +45,7 @@ export default function Home() {
       console.log('Not Authenticated !');
     }
   });
+
   return (
     <>
       <Navbar />
@@ -30,6 +61,29 @@ export default function Home() {
           <Link href="signin">
             <a className="btn"> Sign In</a>
           </Link>
+        </div>
+        <div className="posts">
+          {DUMMY_DB.map((post) => (
+            <div className="post">
+              <img src={post.image} alt="" />
+              <div className="post-actions">
+                <div>
+                  <li>
+                    <i className="fa-solid fa-heart"></i>
+                    {post.stats.likes} likes
+                  </li>
+                  <li>
+                    <i className="fa-solid fa-comment"></i>
+                    {post.stats.comments.length} comments
+                  </li>
+                </div>
+
+                <li className="right">
+                  <i className="fa-solid fa-share"></i>
+                </li>
+              </div>
+            </div>
+          ))}
         </div>
       </main>
     </>
