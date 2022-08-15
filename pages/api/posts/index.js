8240ddcfe11205f00f0b4282c98c0776d1ddc1a0
mohamedMallahi <kingcoding17@gmail.com>
2022-08-15 15:50:31 +0100

    1 file modified

--- pages/api/posts/index.js
+++ pages/api/posts/index.js
@@ -16,20 +16,20 @@ export default async (req, res) => {
     res.status(200).json({ posts: newPosts });
   } else if (req.method === 'POST') {
     console.log(req.body);
-    const { image, caption } = req.body;
-    const storageRef = ref(storage, `images/${image.name}`);
-    const snapshot = await uploadBytes(storageRef, image);
-    console.log('Uploaded an image!');
-    const imageUrl = await getDownloadURL(storageRef);
-    await addDoc(collection(db, 'posts'), {
-      caption: caption,
-      imageUrl: imageUrl,
-      user: user.uid,
-      stats: {
-        likes: [],
-        comments: [],
-      },
-    });
-    res.status(200).json({ message: 'created successfully' });
+    // const { image, caption } = req.body;
+    // const storageRef = ref(storage, `images/${image.name}`);
+    // const snapshot = await uploadBytes(storageRef, image);
+    // console.log('Uploaded an image!');
+    // const imageUrl = await getDownloadURL(storageRef);
+    // await addDoc(collection(db, 'posts'), {
+    //   caption: caption,
+    //   imageUrl: imageUrl,
+    //   user: user.uid,
+    //   stats: {
+    //     likes: [],
+    //     comments: [],
+    //   },
+    // });
+    res.status(200).json(req.body);
   }
 };
