8fc4d8694d354d11854a67ae70bcd699d9af6d1e
mohamedMallahi <kingcoding17@gmail.com>
2022-08-16 11:05:20 +0100

    1 file modified

--- pages/api/posts/index.js
+++ pages/api/posts/index.js
@@ -1,14 +1,8 @@
 // import { NextApiRequest, NextApiResponse } from 'next';
-import formidable from 'formidable';
+// import formidable from 'formidable';
 import { db, storage } from '../../../config/firebase';
 import { collection, getDocs, query, addDoc } from 'firebase/firestore';
 import { ref, uploadBytes, getDownloadURL } from 'firebase/storage';
-
-export const config = {
-  api: {
-    bodyParser: false,
-  },
-};
 
 export default async (req, res) => {
   if (req.method === 'GET') {
@@ -23,31 +17,24 @@ export default async (req, res) => {
     });
     res.status(200).json({ posts: newPosts });
   } else if (req.method === 'POST') {
-    const data = await new Promise((resolve, reject) => {
-      const form = new formidable();
-
-      form.parse(req, (err, fields, files) => {
-        if (err) reject({ err });
-        resolve({ err, fields, files });
-      });
-    });
-
-    // const storageRef = ref(storage, `images/${image.name}`);
-    // const snapshot = await uploadBytes(storageRef, image);
-    // console.log('Uploaded an image!');
-    // const imageUrl = await getDownloadURL(storageRef);
-    // await addDoc(collection(db, 'posts'), {
-    //   caption: caption,
-    //   imageUrl: imageUrl,
-    //   user: user.uid,
-    //   stats: {
-    //     likes: [],
-    //     comments: [],
-    //   },
-    // });
+    const caption = req.body.get('caption');
     res.status(200).json({
       status: 'ok',
-      data,
+      caption,
     });
   }
 };
+
+// const storageRef = ref(storage, `images/${image.name}`);
+// const snapshot = await uploadBytes(storageRef, image);
+// console.log('Uploaded an image!');
+// const imageUrl = await getDownloadURL(storageRef);
+// await addDoc(collection(db, 'posts'), {
+//   caption: caption,
+//   imageUrl: imageUrl,
+//   user: user.uid,
+//   stats: {
+//     likes: [],
+//     comments: [],
+//   },
+// });
